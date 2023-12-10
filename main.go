e06840627690be5efe1395658e6d0f353acfab63
liucan <444346216@qq.com>
2023-12-10 19:42:13 +0800

    change password

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var password = "qaqaqqaqq"
+	var password = "ed4b76e52f61a1fc8f3d997815aaafcf731a2f78"
 	fmt.Printf("password:%s", password)
 	got := animal.Cat()
 	fmt.Println(got)
